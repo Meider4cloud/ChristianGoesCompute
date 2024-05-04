e6bfc2d8d808502dba29772e5aca07e0f7cc9b44
Marcus Eder <m.eder1981@gmail.com>
2024-05-04 20:48:37 +0200

    javascript pdf added

---
+++ challenges/challenge2.js
@@ -0,0 +1,17 @@
+/*
+2. Print a table containing multiplication tables
+
+Let’s start with the tables that many of us had to memorize in school. Can you print a table that contains all the answers 
+to the multiplication tables from 1 through 10?
+
+Like Challenge #1, can you create an efficient solution that you could easily expand should you need the 12 times table?
+
+*/
+
+var table = []
+
+for (let i = 1; i <= 10; i++) {
+    
+}
+
+console.log(table);
